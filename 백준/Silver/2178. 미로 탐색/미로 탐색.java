39fd895d509dc0c495de350ad31450988656c351
HanuKim <117060618+HanuKim@users.noreply.github.com>
2026-03-02 18:49:52 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14724 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+/* 미로 탐색 : bfs의 전형! */
+public class Main {
+    static int N, M;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {1, -1, 0, 0};
+    public static void main(String[] args) throws Exception {
+        // (1,1)에서 출발해서 (N,M)까지 이동하는 최소 칸의 수
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        bfs();
+
+        System.out.println(map[N-1][M-1]);
+    }
+
+    public static void bfs() {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0, 0});
+        visited[0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+
+            for (int d = 0; d < 4; d++) {
+                int nx = cur[0] + dx[d];
+                int ny = cur[1] + dy[d];
+
+                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+                if (visited[nx][ny]) continue;
+                if (map[nx][ny] == 0) continue;
+
+                visited[nx][ny] = true;
+                map[nx][ny] = map[cur[0]][cur[1]] + 1;
+
+                q.offer(new int[]{nx, ny});
+            }
+        }
+    }
+}
