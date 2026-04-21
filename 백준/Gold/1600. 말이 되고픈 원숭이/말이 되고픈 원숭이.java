7f074bb8e2387fb08fe5f0a15b143d61ccdeb254
HanuKim <117060618+HanuKim@users.noreply.github.com>
2026-04-22 01:08:27 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 948 ms, Memory: 104880 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -9,57 +9,54 @@ public class Main {
 
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[] hx = {-1, -2, 1, 2, -2, -1, 1, 2};
-    static int[] hy = {-2, -1, -2, -1, 1, 2, 2, 1};
+    static int[] hx = {-1, -2, 1, 2, -2, -1, 2, 1};
+    static int[] hy = {-2, -1, -2, -1, 1, 2, 1, 2};
 
     static class Node {
-        int x, y, horse, len;
-
-        Node(int x, int y, int horse, int len) {
+        int x, y, k, dist;
+        Node (int x, int y, int k, int dist) {
             this.x = x;
             this.y = y;
-            this.horse = horse;
-            this.len = len;
+            this.k = k;
+            this.dist = dist;
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         K = sc.nextInt();
         W = sc.nextInt();
         H = sc.nextInt();
-
         map = new int[H][W];
-        visited = new boolean[H][W][K + 1];
+        visited = new boolean[H][W][K+1];
 
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
                 map[i][j] = sc.nextInt();
             }
         }
-        System.out.println(bfs());
+
+        System.out.println(dfs());
     }
 
-    static int bfs() {
+    static int dfs() {
         Queue<Node> q = new ArrayDeque<>();
         q.offer(new Node(0, 0, K, 0));
         visited[0][0][K] = true;
 
         while (!q.isEmpty()) {
             Node cur = q.poll();
-            if (cur.x == H - 1 && cur.y == W - 1) {
-                return cur.len;
+            if (cur.x == H-1 && cur.y == W-1) {
+                return cur.dist;
             }
 
-            if (cur.horse > 0) {
-                for (int h = 0; h < 8; h++) {
-                    int nhx = cur.x + hx[h];
-                    int nhy = cur.y + hy[h];
-                    if (nhx < 0 || nhy < 0 || nhx >= H || nhy >= W) continue;
-                    if (map[nhx][nhy] == 1) continue;
-                    if (visited[nhx][nhy][cur.horse - 1]) continue;
-                    visited[nhx][nhy][cur.horse - 1] = true;
-                    q.offer(new Node(nhx, nhy, cur.horse - 1, cur.len + 1));
+            if (cur.k > 0) {
+                for (int d = 0; d < 8; d++) {
+                    int nx = cur.x + hx[d];
+                    int ny = cur.y + hy[d];
+                    if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
+                    if (map[nx][ny] == 1 || visited[nx][ny][cur.k - 1]) continue;
+                    visited[nx][ny][cur.k - 1] = true;
+                    q.offer(new Node(nx, ny, cur.k - 1, cur.dist + 1));
                 }
             }
 
@@ -67,13 +64,11 @@ public class Main {
                 int nx = cur.x + dx[d];
                 int ny = cur.y + dy[d];
                 if (nx < 0 || ny < 0 || nx >= H || ny >= W) continue;
-                if (map[nx][ny] == 1) continue;
-                if (visited[nx][ny][cur.horse]) continue;
-                visited[nx][ny][cur.horse] = true;
-                q.offer(new Node(nx, ny, cur.horse, cur.len + 1));
+                if (map[nx][ny] == 1 || visited[nx][ny][cur.k]) continue;
+                visited[nx][ny][cur.k] = true;
+                q.offer(new Node(nx, ny, cur.k, cur.dist + 1));
             }
         }
-
         return -1;
     }
 }
